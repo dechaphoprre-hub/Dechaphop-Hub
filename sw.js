fbc880fba8116d8c6659320b8b2a2f5689d499c2
dechaphoprre-hub <dechaphop.rre@gmail.com>
2026-04-23 00:23:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'personal-hub-v1';
+const CACHE_NAME = 'personal-hub-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js'
 ];
 
